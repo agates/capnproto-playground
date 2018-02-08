ff55f46faa635d74b76cd3ec304756bff1273556
Alecks Gates <alecks.g@gmail.com>
2018-02-08 01:29:31 -0600

    Make service browsing a little more generic and resilient. Migrate zeroconf service properties to use a Cap'n Proto schema. Some more error handling and locking due to zeroconf threading.

--- src/main.py
+++ src/main.py
@@ -1,36 +1,67 @@
 #!/usr/bin/env python3
 
 from datetime import datetime, timezone
-import pprint
+import signal
 import socket
+import sys
 import time
+from threading import Lock
 
 from zeroconf import ServiceBrowser, Zeroconf
 
+from schema.data_pathway import DataPathway
 from schema.ph_event import PhEvent
 
 
-class PhEventCapnprotoListener:
-    def __init__(self):
-        self.endpoints = []
+def extract_data_pathway(info):
+    try:
+        return DataPathway.loads(info.properties[b'data-pathway'])
+    except KeyError:
+        raise KeyError(
+            "'data-pathway' does not exist in service properties on '{0}', {1}:{2}, {3}".format(
+                info.name, socket.inet_ntoa(info.address), info.server, info.port
+            ))
 
-    def remove_service(self, zeroconf, type, name):
-        info = zeroconf.get_service_info(type, name)
-        pprint.pprint(info)
-        if info.properties[b'type'] == b'PhEvent':
-            self.endpoints.remove((socket.inet_ntoa(info.address), info.port))
+
+class Browser:
+    def __init__(self, capnproto_struct):
+        self.capnproto_struct = capnproto_struct
+        self.struct_name = bytes(capnproto_struct.__name__, "UTF-8")
+
+        # Keep an internal set for uniqueness, tuple for faster iteration
+        # Lock is required because zeroconf is threaded
+        self._endpoints = tuple()
+        self._endpoints_dict = {}
+        self._endpoints_lock = Lock()
+
+    @property
+    def endpoints(self):
+        with self._endpoints_lock:
+            return self._endpoints
+
+    def add_endpoint(self, name, e):
+        with self._endpoints_lock:
+            self._endpoints_dict[name] = e
+            self.flatten_endpoints()
+
+    def remove_endpoint(self, name):
+        with self._endpoints_lock:
+            del self._endpoints_dict[name]
+            self.flatten_endpoints()
+
+    def flatten_endpoints(self):
+        self._endpoints = tuple(self._endpoints_dict.values())
 
     def add_service(self, zeroconf, type, name):
         info = zeroconf.get_service_info(type, name)
 
-        pprint.pprint(info)
-        if info.properties[b'type'] == b'PhEvent':
-            self.endpoints.append((socket.inet_ntoa(info.address), info.port))
+        data_pathway = extract_data_pathway(info)
 
+        if data_pathway.struct_name == self.struct_name:
+            self.add_endpoint(name, (socket.inet_ntoa(info.address), info.port))
 
-zeroconf = Zeroconf()
-listener = PhEventCapnprotoListener()
-browser = ServiceBrowser(zeroconf, "_capnproto._udp.local.", listener)
+    def remove_service(self, zeroconf, type, name):
+        self.remove_endpoint(name)
 
 
 def current_timestamp():
@@ -42,6 +73,12 @@ def current_timestamp_nanoseconds():
     return current_timestamp() * 10 ** 9
 
 
+signal.signal(signal.SIGINT, signal.default_int_handler)
+
+zeroconf = Zeroconf()
+phevent_browser = Browser(PhEvent)
+zeroconf_browser = ServiceBrowser(zeroconf, "_data-pathway._udp.local.", phevent_browser)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
@@ -51,12 +88,14 @@ try:
         print(ph_event)
         ph_message = ph_event.dumps()
 
-        for endpoint in listener.endpoints:
+        for endpoint in phevent_browser.endpoints:
             r = sock.sendto(ph_message, endpoint)
 
         time.sleep(1)
 except KeyboardInterrupt:
     pass
 finally:
-    sock.close()
+    zeroconf_browser.cancel()
     zeroconf.close()
+    sock.close()
+    sys.exit()
